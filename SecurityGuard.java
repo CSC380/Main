e8329121aaeb0a0f2db87a180fc69f5dbc211352
smiglior <smiglior@oswego.edu>
2017-05-07 23:18:06 -0400

    Update SecurityGuard.java

--- SecurityGuard.java
+++ SecurityGuard.java
@@ -1,12 +1,10 @@
 
 public class SecurityGuard extends Employee {
 
-private final double wage = 18.00;
+
 private final String mainGate = "Main Gate";
-private String [] schedule = new String[7];
 private boolean stateOfGate;
 
-private double hoursWorked, weeklyHours;
 
 private Customer customerInfo;
 private SecurityGuard sc;
@@ -42,34 +40,7 @@ public SecurityGuard getMainGateEMP() {
 return sc;
 }
 
-public double calculatePayCheck() {
 
-return hoursWorked * wage;
-}
-
-public String printPayCheck() {
-return f.format(calculatePayCheck());
-}
-
-public void setWeeklyHours(double wh) {
-
-weeklyHours = wh;
-}
-
-public double getWeeklyHours() {
-
-return weeklyHours;
-}
-
-public void setHoursWorked(double hw) {
-
-hoursWorked = hw;
-}
-
-public double getHoursWorked() {
-
-return hoursWorked;
-}
 
 public void setCustomerInfo(Customer ci) {
 
@@ -81,24 +52,7 @@ public Customer getCustomerInfo() {
 return customerInfo;
 }
 
-public void setSchedule(String mon, String tues, String wed, String thurs, String fri, String sat, String sun) {
 
-schedule[0] = "Monday - " + mon;
-schedule[1] = "Tuesday - " + tues;
-schedule[2] = "Wednesday - " + wed;
-schedule[3] = "Thursday - " + thurs;
-schedule[4] = "Friday - " + fri;
-schedule[5] = "Saturday - " + sat;
-schedule[6] = "Sunday - " + sun;
-}
-
-public void displaySchedule() {
-
-for (int i = 0; i < schedule.length; i++) {
-	System.out.println(schedule[i]);
-}
-
-}
 
 public static void main(String[] args) {
 /*
