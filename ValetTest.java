f7aa983864bf23962e04fc008edd7bfa7f700073
smiglior <smiglior@oswego.edu>
2017-05-07 23:23:30 -0400

    Update ValetTest.java

--- ValetTest.java
+++ ValetTest.java
@@ -17,31 +17,7 @@ import static org.junit.Assert.*;
  */
 public class ValetTest {
     
-    @Test
-    public void getWeeklyHours(){
-        Valet v1 = new Valet("Foster", "Will", "gv1234", "8457454254");
-        v1.setWeeklyHours(30);
-        assertEquals("Checks to see if weekly hours are set", 30, v1.getWeeklyHours(),0);
-    }
-    @Test
-    public void geHoursWorked(){
-        Valet v1 = new Valet("Foster", "Will", "gv1234", "8457454254");
-        v1.setHoursWorked(30);
-        assertEquals("Checks to see if hours worked  are set", 30, v1.getHoursWorked(0),0);
-    }
-    @Test
-    public void calculatePaycheck(){
-         Valet v1 = new Valet("Foster", "Will", "gv1234", "8457454254");
-         v1.setHoursWorked(30);
-         assertEquals(450,v1.calculatePayCheck(),0);
-    }
-    @Test
-    public void printPaycheck(){
-         Valet v1 = new Valet("Foster", "Will", "gv1234", "8457454254");
-         v1.setHoursWorked(30);
-         String s = "450.00";
-         assertEquals(s, v1.printPayCheck());
-    }
+ 
     @Test
     public void getCustomerInfo(){
         Customer c1 = new Customer("Migliori", "Samantha","migliorise@gmail.com", "LFG7854");
