0b8cc097284cbe84ebd6aa9df2467044ca5e2752
smiglior <smiglior@oswego.edu>
2017-05-07 23:26:57 -0400

    Update SecurityGuardTest.java

--- SecurityGuardTest.java
+++ SecurityGuardTest.java
@@ -45,59 +45,6 @@ public class SecurityGuardTest {
 	assertEquals("Checking for the main gate emp to have been set",g1,g1.getMainGateEMP());
 	}
 
-	@Test
-	public void calculatePayCheckTest() {
-
-	SecurityGuard g1 = new SecurityGuard("Law","Thomas","8765556","34577776545");
-	g1.setHoursWorked(40);
-	assertEquals("Checking for calculated paycheck",720,g1.calculatePayCheck(),.01);
-	}
-
-	@Test
-	public void printPayCheckTest() {
-
-	SecurityGuard g1 = new SecurityGuard("Law","Thomas","8765556","34577776545");
-	g1.setHoursWorked(40);
-	String pay = "720.00";
-	assertEquals("Checking for calculated paycheck output match",pay,g1.printPayCheck());
-	}
-/*
-	@Test
-	public void setWeeklyHoursTest() {
-
-	SecurityGuard g1 = new SecurityGuard("Law","Thomas","8765556","34577776545");
-	g1.setWeeklyHours(40);
-	double hours = 40;
-	assertEquals("Checking for hours to match",hours,g1.getWeeklyHours(),0);
-	}
-*/
-	@Test
-	public void getWeeklyHoursTest() {
-
-	SecurityGuard g1 = new SecurityGuard("Law","Thomas","8765556","34577776545");
-	g1.setWeeklyHours(40);
-	double hours = 40;
-	assertEquals("Checking for hours to match",hours,g1.getWeeklyHours(),0);
-	}
-/*
-	@Test
-	public void setHoursWorkedTest() {
-
-	SecurityGuard g1 = new SecurityGuard("Law","Thomas","8765556","34577776545");
-	g1.setHoursWorked(40);
-	double worked = 40;
-	assertEquals("Checking for hours worked",worked,g1.getHoursWorked(),0);
-	}
-*/
-	@Test
-	public void getHoursWorkedTest() {
-
-	SecurityGuard g1 = new SecurityGuard("Law","Thomas","8765556","34577776545");
-	g1.setHoursWorked(40);
-	double worked = 40;
-	assertEquals("Checking for hours worked",worked,g1.getHoursWorked(),0);
-	}
-}
 
 /*
 
